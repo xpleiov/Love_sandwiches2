f99077dc503dbb2e1b20c01263ff873c56cc9ee3
xpleiov <Sunny.helset@outlook.com>
2022-05-24 21:21:05 +0000

    Add updating excel sheet function

--- run.py
+++ run.py
@@ -30,7 +30,7 @@ def get_sales_data():
         if validate_data(sales_data):
             print("data is valid")
             break
-
+    return sales_data
 
 def validate_data(values):
     """
@@ -50,4 +50,16 @@ def validate_data(values):
 
     return True
 
-get_sales_data()
+def update_sales_worksheet(data):
+    """
+    Update sales worksheet, add new row with the list data provided.
+    """
+    print("updating sales worksheet..\n")
+    sales_worksheet = SHEET.worksheet("sales")
+    sales_worksheet.append_row(data)
+    print("Sales worksheet updated succesfully...\n")
+
+
+data = get_sales_data()
+sales_data = [int(num) for num in data]
+update_sales_worksheet(sales_data)
